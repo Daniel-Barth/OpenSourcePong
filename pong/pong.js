eab4061a2da00a0a16d4a079262919b44e9b59cb
Dareen Alhyari <dareen.alhiyari@gmail.com>
2024-03-29 00:52:41 +0100

    smooth movement of paddles and better ball bouncing

--- pong/pong.js
+++ pong/pong.js
@@ -5,7 +5,30 @@ const ctx = canvas.getContext("2d");
 // Setting dimensions of canvas
 const canvasWidth = 700;
 const canvasHeight = 500;
+
+// velocity
+let ballVelocity = 0.5;
+let paddlesVelocity = 0.0005
+
+///// ball
+// position
+let ballPosX = 500;
+let ballPosY = 50;
+// size
 const radius = 10;
+// velocity controls
+let ballVelocityX = ballVelocity;
+let ballVelocityY = ballVelocity;
+
+///// paddles
+// position
+let leftPaddleY = 20;
+let leftPaddleX = 15;
+let rightPaddleY = 20;
+let rightPaddleX = 665;
+// size
+const paddleHeight = 90;
+const paddleWidth = 20;
 
 // render rect
 function renderBackground() {
@@ -13,8 +36,7 @@ function renderBackground() {
   ctx.fillRect(0, 0, canvasWidth, canvasHeight);
 }
 
-const paddleHeight = 90;
-const paddleWidth = 20;
+
 function renderPaddle(x, y, color) {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, paddleWidth, paddleHeight);
@@ -24,106 +46,125 @@ function renderPaddle(x, y, color) {
 function renderBall(x, y) {
   ctx.beginPath();
   ctx.arc(x, y, radius, 0, 2 * Math.PI);
-  ctx.fillStyle = "blue";
+  ctx.fillStyle = "cyan";
   ctx.fill();
 }
 
-// ball
-let ballPosX = 500;
-let ballPosY = 50;
-let ballVelocityX = +2;
-let ballVelocityY = +2;
-// paddles
-let leftPaddleY = 20;
-let leftPaddleX = 35;
-let rightPaddleY = 20;
-let rightPaddleX = 35;
-let paddlesVelocity = 10
-
+let speedLeft = 0;
+let leftDirection = +1
+let reduceSpeedLeft = true;
+let speedRight = 0;
+let rightDirection = +1
+let reduceSpeedRight = true;
 document.addEventListener("keypress", (event) => {
+  // left keys
   if (event.key === "s") {
-    leftPaddleY += paddlesVelocity;
+    reduceSpeedLeft = false;
+    leftDirection = +1;
   }
   if (event.key === "w") {
-    leftPaddleY -= paddlesVelocity;
+    reduceSpeedLeft = false;
+    leftDirection = -1;
   }
-  console.log(leftPaddleY);
-  console.log(rightPaddleY);
-});
 
-document.addEventListener("keypress", (event) => {
+  // right keys
   if (event.key === "k") {
-    rightPaddleY += paddlesVelocity;
+    reduceSpeedRight = false;
+    rightDirection = +1;
   }
   if (event.key === "o") {
-    rightPaddleY -= paddlesVelocity;
+    reduceSpeedRight = false;
+    rightDirection = -1;
   }
-  console.log(leftPaddleY);
-  console.log(rightPaddleY);
+});
+document.addEventListener("keyup", (event) => {
+  
+  if (event.key === "s" || event.key === "w") {
+    reduceSpeedLeft = true;
+  }
+  if (event.key === "k" || event.key === "o") {
+    reduceSpeedRight = true;
+  }
 });
 
+// helper functions
+function isBallTouchingLeftPaddle() {
+  return ballPosX - radius <= leftPaddleX + paddleWidth && ballPosY >= leftPaddleY && ballPosY <= leftPaddleY + paddleHeight;
+}
 
+function isBallTouchingRightPaddle() {
+  return ballPosX + radius >= rightPaddleX && ballPosY >= rightPaddleY && ballPosY <= rightPaddleY + paddleHeight;
+}
+
+function isBallTouchingLeftWall() {
+  return ballPosX - radius === 0;
+}
+
+function isBallTouchingRightWall() {
+  return ballPosX + radius === canvasWidth;
+}
+
+
+function bounceBall() {
+  if (isBallTouchingRightWall() || isBallTouchingRightPaddle()) {
+    ballVelocityX = -ballVelocity;
+  }
+  if (isBallTouchingLeftWall() || isBallTouchingLeftPaddle()) {
+    ballVelocityX = ballVelocity;
+  }
+  if (ballPosY + radius === canvasHeight) {
+    ballVelocityY = -ballVelocity;
+  }
+  if (ballPosY - radius === 0) {
+    ballVelocityY = ballVelocity;
+  }
+}
+
+
+// game loop
 setInterval(() => {
   renderBackground();
   renderBall(ballPosX, ballPosY);
 
-  renderPaddle(15, leftPaddleY, "green");
-  renderPaddle(665, rightPaddleY, "red");
+  renderPaddle(leftPaddleX, leftPaddleY, "green");
+  renderPaddle(rightPaddleX, rightPaddleY, "red");
 
   // move the ball
   ballPosX += ballVelocityX;
   ballPosY += ballVelocityY;
-  // if (ballPosX + radius === canvasWidth || ballPosX - radius === 0) {
-  //   ballVelocityX = -ballVelocityX
-  // }
-
-  // bounce the ball when it reaches the canvas borders
-  if (ballPosX + radius === canvasWidth) {
-    ballVelocityX = -2;
-  }
-  if (ballPosX - radius === 0) {
-    ballVelocityX = +2;
-  }
-  if (ballPosY + radius === canvasHeight) {
-    ballVelocityY = -2;
-  }
-  if (ballPosY - radius === 0) {
-    ballVelocityY = +2;
-  }
-  /*
-  if (ballPosY - radius === leftPaddleY + paddleHeight && ballPosX + radius === leftPaddleX +paddleWidth) {
-    ballVelocityX = +2
-    ballVelocityY = +2
-  }
-
-  if (ballPosY - radius >= secondPaddleHeight && ballPosY + radius <= secondPaddleHeight) {
-    ballVelocityY = +2
-  }
-  if (ballPosX - radius >= secondPaddleWidth && ballPosX + radius <= secondPaddleWidth) {
-    ballVelocityX = +2
-  } 
-
-}, 17); 
-*/
-  // if (ballPosX + radius === leftPaddleY && 20) {
-  //   ballVelocityX = -2
-  // }
-  // // if (ballPosX - radius === 0) {
-  // //   ballVelocityX = +2
-  // // }
-  // // }, 17);
   
-  // create borders for the paddles
-  if (leftPaddleY <= 0) {
-    leftPaddleY = 0;
-  } else if (leftPaddleY >= 500) {
-    leftPaddleY = 500;
+  // bounce the ball off the paddles and canvas borders
+  bounceBall();
+
+  // move paddles 
+  leftPaddleY += leftDirection * 15 * Math.sin(speedLeft);
+  rightPaddleY += rightDirection * 15 * Math.sin(speedRight);
+  if (reduceSpeedLeft) {
+    speedLeft -= paddlesVelocity; if (speedLeft < 0) {speedLeft = 0;} 
+  } else {
+    speedLeft += paddlesVelocity; if (speedLeft >= 1.6) {speedLeft = 1.6}
+  }
+  if (reduceSpeedRight) {
+    speedRight -= paddlesVelocity; if (speedRight < 0) {speedRight = 0;}
+  } else {
+    speedRight += paddlesVelocity; if (speedRight >= 1.6) {speedRight = 1.6}
   }
 
-  if (rightPaddleY <= 0) {
+  // create borders for the paddles
+  if (leftPaddleY < 0) {
+    leftPaddleY = 0;
+    speedLeft = 0;
+  } else if (leftPaddleY + paddleHeight > canvasHeight) {
+    leftPaddleY = canvasHeight - paddleHeight;
+    speedLeft = 0;
+  }
+
+  if (rightPaddleY < 0) {
     rightPaddleY = 0;
-  } else if (rightPaddleY >= 500) {
-    rightPaddleY = 500;
+    speedRight = 0;
+  } else if (rightPaddleY + paddleHeight > canvasHeight) {
+    rightPaddleY = canvasHeight - paddleHeight;
+    speedRight = 0;
   }
 
 });
